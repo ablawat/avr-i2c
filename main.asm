5f7f997b72d1bd1fa5a9b2ebdc9d7f70942ec9b6
Adam Bławat <blawat.adam@gmail.com>
2016-02-22 23:05:44 +0100

    Added additional register definitions.

--- main.asm
+++ main.asm
@@ -2,19 +2,23 @@
                 .INCLUDE "twi-def.inc"
                 .INCLUDE "i2c-def.inc"
                 
-                .EQU EEPROM_DEV_ADDR = 0xA0
-                
                 .EQU EEPROM_ADDR_START = 0x0200
                 .EQU EEPROM_ADDR_SHIFT = 4
                 
+                ; --- Timeouts ---
                 .EQU WAIT_TIME1_MS = 500
                 .EQU WAIT_TIME2_MS = 250
                 
+                ; --- EEPROM Register Defines ---
                 .DEF EEPROM_DATA = r0
                 
-                .DEF EEPROM_ADDR_L = r26
-                .DEF EEPROM_ADDR_H = r27
+                .DEF EEPROM_ADDR_L = r2
+                .DEF EEPROM_ADDR_H = r3
                 
+                .DEF ADDR_SHIFT_L = r4
+                .DEF ADDR_SHIFT_H = r5
+                
+                ; --- Debug Output Pins ---
                 .EQU DEBUG_PIN1 = PORTD6
                 .EQU DEBUG_PIN2 = PORTD7
                 
@@ -43,75 +47,77 @@
                 
                 ; --- Load Address Shift Value ---
                 ldi     r16,  LOW(EEPROM_ADDR_SHIFT)
-                mov     r2, r16
+                mov     ADDR_SHIFT_L, r16
                 ldi     r16, HIGH(EEPROM_ADDR_SHIFT)
-                mov     r3, r16
+                mov     ADDR_SHIFT_H, r16
                 
                 ; --- Load Byte Start Address ---
-                ldi     EEPROM_ADDR_L,  LOW(EEPROM_ADDR_START)
-                ldi     EEPROM_ADDR_H, HIGH(EEPROM_ADDR_START)
+                ldi     r16,  LOW(EEPROM_ADDR_START)
+                mov     EEPROM_ADDR_L, r16
+                ldi     r16, HIGH(EEPROM_ADDR_START)
+                mov     EEPROM_ADDR_H, r16
                 
                 ; --- Set Byte Initial Value ---
                 clr     EEPROM_DATA
                 
-                ; --- Turn On Debug Indications ---
+                ; --- Set Debug Pins to High ---
 main_start:     sbi     PORTD, DEBUG_PIN1
                 sbi     PORTD, DEBUG_PIN2
                 
-                ; --- Wait 1 second ---
+                ; --- Wait ---
                 ldi     r24,  LOW(WAIT_TIME2_MS)
                 ldi     r25, HIGH(WAIT_TIME2_MS)
                 rcall   delay_ms
                 
-                ; --- Turn Off Debug Indications ---
+                ; --- Set Debug Pins to Low ---
                 cbi     PORTD, DEBUG_PIN1
                 cbi     PORTD, DEBUG_PIN2
                 
-                ; --- Wait 1 second ---
+                ; --- Wait ---
                 ldi     r24,  LOW(WAIT_TIME2_MS)
                 ldi     r25, HIGH(WAIT_TIME2_MS)
                 rcall   delay_ms
                 
                 ; --- Write Byte to EEPROM ---
-                mov     r18, EEPROM_DATA
-                mov     r24, EEPROM_ADDR_L
-                mov     r25, EEPROM_ADDR_H
+                mov     I2C_ARG_ADDR_L, EEPROM_ADDR_L
+                mov     I2C_ARG_ADDR_H, EEPROM_ADDR_H
+                mov     I2C_ARG_DATA, EEPROM_DATA
                 rcall   i2c_eeprom_write_byte
                 
                 ; --- Check Return Status ---
-                cpi     r22, I2C_STATUS_OK
+                cpi     I2C_RET_STAT, I2C_STATUS_OK
                 breq    main_W1
                 sbi     PORTD, DEBUG_PIN1
                 
-                ; --- Wait 1 second ---
+                ; --- Wait ---
 main_W1:        ldi     r24,  LOW(WAIT_TIME1_MS)
                 ldi     r25, HIGH(WAIT_TIME1_MS)
                 rcall   delay_ms
                 
                 ; --- Overwrite Byte Value ---
-                neg     r18
+                neg     I2C_ARG_DATA
                 
                 ; --- Read Byte from EEPROM ---
-                mov     r24, EEPROM_ADDR_L
-                mov     r25, EEPROM_ADDR_H
+                mov     I2C_ARG_ADDR_L, EEPROM_ADDR_L
+                mov     I2C_ARG_ADDR_H, EEPROM_ADDR_H
                 rcall   i2c_eeprom_read_byte
                 
                 ; --- Check Return Status ---
-                cpi     r22, I2C_STATUS_OK
+                cpi     I2C_RET_STAT, I2C_STATUS_OK
                 breq    main_W2
                 sbi     PORTD, DEBUG_PIN1
                 
-                ; --- Wait 1 second ---
+                ; --- Wait ---
 main_W2:        ldi     r24,  LOW(WAIT_TIME1_MS)
                 ldi     r25, HIGH(WAIT_TIME1_MS)
                 rcall   delay_ms
                 
                 ; --- Compare EEPROM Values ---
-                cp      r18, EEPROM_DATA
-                brne    main_W3
-                sbi     PORTD, DEBUG_PIN2
+                cp      I2C_ARG_DATA, EEPROM_DATA
+                breq    main_W3
+                sbi     PORTD, DEBUG_PIN1
                 
-                ; --- Wait 1 second ---
+                ; --- Wait ---
 main_W3:        ldi     r24,  LOW(WAIT_TIME1_MS)
                 ldi     r25, HIGH(WAIT_TIME1_MS)
                 rcall   delay_ms
@@ -120,8 +126,8 @@ main_W3:        ldi     r24,  LOW(WAIT_TIME1_MS)
                 inc     EEPROM_DATA
                 
                 ; --- Change EEPROM Byte Address ---
-                add     EEPROM_ADDR_L, r2
-                adc     EEPROM_ADDR_H, r3
+                add     EEPROM_ADDR_L, ADDR_SHIFT_L
+                adc     EEPROM_ADDR_H, ADDR_SHIFT_H
                 
                 rjmp    main_start
                 
