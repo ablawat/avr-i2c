dfa122b7a2f3afa04d933a2d773c2f09d1d649ff
Adam Bławat <blawat.adam@gmail.com>
2016-03-24 23:00:24 +0100

    Added handles for Hardware Statuses TWI_MT_DATA_SENT_ACK, TWI_MR_DATA_RECV_ACK and TWI_MR_DATA_RECV_NACK.

--- eeprom-manager-2/i2c-eeprom-statemachine.asm
+++ eeprom-manager-2/i2c-eeprom-statemachine.asm
@@ -13,7 +13,8 @@
                                 in      I2C_SM_HSTAT, TWSR
                                 
                                 ; --- [CASE] START Condition was sent ---
-i2c_eeprom_statemachine_ST1:    cpi     I2C_SM_HSTAT, TWI_START_SENT
+i2c_eeprom_statemachine_ST1:    ldi     r16, TWI_START_SENT
+                                cp      r16, I2C_SM_HSTAT
                                 brne    i2c_eeprom_statemachine_ST2
                                 
                                 ; --- Change Communication State ---
@@ -30,7 +31,8 @@ i2c_eeprom_statemachine_ST1:    cpi     I2C_SM_HSTAT, TWI_START_SENT
                                 rjmp    i2c_eeprom_statemachine_END
                                 
                                 ; --- [CASE] Repeated START Condition was sent ---
-i2c_eeprom_statemachine_ST2:    cpi     I2C_SM_HSTAT, TWI_RESTART_SENT
+i2c_eeprom_statemachine_ST2:    ldi     r16, TWI_RESTART_SENT
+                                cp      r16, I2C_SM_HSTAT
                                 brne    i2c_eeprom_statemachine_ST3
                                 
                                 ; --- Change Communication State ---
@@ -47,7 +49,8 @@ i2c_eeprom_statemachine_ST2:    cpi     I2C_SM_HSTAT, TWI_RESTART_SENT
                                 rjmp    i2c_eeprom_statemachine_END
                                 
                                 ; --- [CASE] Slave Addres + Write was sent ---
-i2c_eeprom_statemachine_ST3:    cpi     I2C_SM_HSTAT, TWI_MT_SLA_SENT_ACK
+i2c_eeprom_statemachine_ST3:    ldi     r16, TWI_MT_SLA_SENT_ACK
+                                cp      r16, I2C_SM_HSTAT
                                 brne    i2c_eeprom_statemachine_ST4
                                 
                                 ; --- Change Communication State ---
@@ -55,7 +58,7 @@ i2c_eeprom_statemachine_ST3:    cpi     I2C_SM_HSTAT, TWI_MT_SLA_SENT_ACK
                                 mov     I2C_SM_STATE, r16
                                 
                                 ; --- Send High Byte of EEPROM Address ---
-                                ldi     r16, [[X]]
+                                ldi     r16, I2C_SM_EEPROM_ADDR_H
                                 out     TWDR, r16
                                 ldi     r16, TWI_SEND
                                 out     TWCR, r16
@@ -64,8 +67,9 @@ i2c_eeprom_statemachine_ST3:    cpi     I2C_SM_HSTAT, TWI_MT_SLA_SENT_ACK
                                 rjmp    i2c_eeprom_statemachine_END
                                 
                                 ; --- [CASE] Slave Addres + Read was sent ---
-i2c_eeprom_statemachine_ST4:    cpi     I2C_SM_HSTAT, TWI_MR_SLA_SENT_ACK
-                                brne    [[X]]
+i2c_eeprom_statemachine_ST4:    ldi     r16, TWI_MR_SLA_SENT_ACK
+                                cp      r16, I2C_SM_HSTAT
+                                brne    i2c_eeprom_statemachine_ST5
                                 
                                 ; --- Change Communication State ---
                                 ldi     r16, I2C_STATE_MR_ENTERED
@@ -76,4 +80,43 @@ i2c_eeprom_statemachine_ST4:    cpi     I2C_SM_HSTAT, TWI_MR_SLA_SENT_ACK
                                 
                                 ; --- [BREAK] ---
                                 rjmp    i2c_eeprom_statemachine_END
+                                
+                                ; --- [CASE] Data Byte was sent --- ACK was received ---
+i2c_eeprom_statemachine_ST5:    ldi     r16, TWI_MT_DATA_SENT_ACK
+                                cp      r16, I2C_SM_HSTAT
+                                brne    i2c_eeprom_statemachine_ST6
+                                
+                                ; --- Comment ---
+                                [TBD][Handle State Function]
+                                
+                                ; --- [BREAK] ---
+                                rjmp    i2c_eeprom_statemachine_END
+                                
+                                ; --- [CASE] Data Byte was received --- ACK was sent ---
+i2c_eeprom_statemachine_ST6:    ldi     r16, TWI_MR_DATA_RECV_ACK
+                                cp      r16, I2C_SM_HSTAT
+                                brne    i2c_eeprom_statemachine_ST7
+                                
+                                ; --- Comment ---
+                                [TBD][Handle State Function]
+                                
+                                ; --- [BREAK] ---
+                                rjmp    i2c_eeprom_statemachine_END
+                                
+                                ; --- [CASE] Data Byte was received --- NACK was sent ---
+i2c_eeprom_statemachine_ST7:    ldi     r16, TWI_MR_DATA_RECV_NACK
+                                cp      r16, I2C_SM_HSTAT
+                                brne    [[X]]
+                                
+                                ; --- Change Communication State ---
+                                ldi     r16, I2C_STATE_IDLE
+                                mov     I2C_SM_STATE, r16
+                                
+                                ; --- Send STOP Condition ---
+                                ldi     r16, TWI_STOP
+                                out     TWCR, r16
+                                
+                                ; --- [BREAK] ---
+                                rjmp    i2c_eeprom_statemachine_END
+                                
 i2c_eeprom_statemachine_END:
